66732ac079ab6a4c892ceabeae6a47bedb53ac8f
apisakch <apisakch11@hotmail.com>
2022-08-21 01:21:05 +0700

    updated notice delete

--- src/routers/activities.js
+++ src/routers/activities.js
@@ -24,8 +24,13 @@ router.get('/:user',async (req,res,next)=>{
 
     const myParams = req.params.user
     const myQuery = req.query
-    
-    const activities = await activitiesModel.find({username_id:myParams});
+    var date_start = new Date(myQuery.date_start)
+    var date_end = new Date(myQuery.date_end)
+    date_start.setHours(30,59,59)
+  
+    // console.log(date_start)
+    // console.log(date_end)
+    const activities = await activitiesModel.find({$and:[{date_post:{$lte:date_start}},{date_post:{$gt:date_end}},{"username_id":myParams}]}).sort ( { date_post: -1 } );
     res.send(activities.map((activities)=> activities.toJSON()))
  
 })
