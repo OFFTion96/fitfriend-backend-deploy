f182c6af987c0ad6fde5b695089519963a2e8560
apisakch <apisakch11@hotmail.com>
2022-08-24 10:11:57 +0700

    git

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-
 require('dotenv').config();
 
 module.exports = {
-isVercel: process.env.IS_VERCEL || false,
+  isVercel: process.env.IS_VERCEL || false,
   port: process.env.PORT || 4001,
   mongoUri: process.env.MONGO_URI,
   mongoOptions: {
